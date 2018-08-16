c356b30c3aecc880b3b8edead97a5444dddad931
Matheus Castiglioni <matheushcastiglioni@gmail.com>
2018-08-15 21:44:16 -0300

    Adding new translation messages

--- src/_translate/languages/en.js
+++ src/_translate/languages/en.js
@@ -1,7 +1,11 @@
 const messages = {
     en: {
         translations: {
+            subtitles: {
+                notFound: "The URL doesn't match with none component, check if it is corret or try access another one"
+            },
             titles: {
+                app: "My Reads",
                 notFound: "Page not found"
             }
         }
--- src/_translate/languages/pt.js
+++ src/_translate/languages/pt.js
@@ -1,7 +1,14 @@
 const messages = {
     pt: {
         translations: {
+            buttons: {
+                backHome: "Voltar para a página principal"
+            },
+            subtitles: {
+                notFound: "A URL não combina com nenhum componente, verifique se ela está correta ou tente acessar outro endereço."
+            },
             titles: {
+                app: "My Reads",
                 notFound: "Página não encontrada"
             }
         }
