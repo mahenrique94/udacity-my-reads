6f0212bde38660a3a0402312b41a8006af59d1da
Matheus Castiglioni <matheushcastiglioni@gmail.com>
2018-08-15 21:43:45 -0300

    Importing .css file from bulma components

--- src/_config/style.js
+++ src/_config/style.js
@@ -3,3 +3,5 @@ import "normalize.css"
 import "@fortawesome/fontawesome-svg-core"
 import "@fortawesome/free-solid-svg-icons"
 import "@fortawesome/react-fontawesome"
+
+import "react-bulma-components/dist/react-bulma-components.min.css"
