d1668ef56d0107f224901ed03df412ef837da697
Matheus Castiglioni <matheushcastiglioni@gmail.com>
2018-08-15 21:45:25 -0300

    Start styling Home component

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,5 +1,19 @@
 import React from "react"
 
-const Home = () => <h1>Home</h1>
+import i18n from "i18n"
+
+import { Container, Heading, Hero } from "react-bulma-components"
+
+const Home = () => (
+    <Hero color="info">
+        <Hero.Body>
+            <Container>
+                <Heading>
+                    { i18n.t("titles.app") }
+                </Heading>
+            </Container>
+        </Hero.Body>
+    </Hero>
+)
 
 export default Home
