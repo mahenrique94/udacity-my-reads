6fdb0539fced2db9d259c408d3b1b248cc308011
Matheus Castiglioni <matheushcastiglioni@gmail.com>
2018-08-15 21:44:48 -0300

    Styling NotFound component

--- src/components/notFound/NotFound.js
+++ src/components/notFound/NotFound.js
@@ -1,6 +1,24 @@
 import React from "react"
+import { Link } from "react-router-dom"
+
 import i18n from "i18n"
 
-const NotFound = () => <h1>{ i18n.t("titles.notFound") }</h1>
+import { Container, Heading, Hero } from "react-bulma-components"
+
+const NotFound = () => (
+    <Hero color="danger">
+        <Hero.Body>
+            <Container>
+                <Heading>
+                    { i18n.t("titles.notFound") }
+                </Heading>
+                <Heading renderAs="h2" size={ 4 } subtitle>
+                    { i18n.t("subtitles.notFound") }
+                </Heading>
+                <Link to="/">{ i18n.t("buttons.backHome") }</Link>
+            </Container>
+        </Hero.Body>
+    </Hero>
+)
 
 export default NotFound
